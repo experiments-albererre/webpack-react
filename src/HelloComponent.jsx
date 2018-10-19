b961426f6e68e524d786c2cd8735364b42f8fb1c
Alber Erre <a.ruizblugo@gmail.com>
2018-10-19 20:33:38 +0100

    react webpack working

---
+++ src/HelloComponent.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class HelloComponent extends Component {
+  render() {
+    return (<div>Que pachaa!</div>);
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import HelloComponent from './HelloComponent';
+
+render(<HelloComponent/>, document.getElementById('app'));
